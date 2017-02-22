f80f274cfb67c6e7bdb0bb26d31242a600b42b57
coco <308695699@qq.com>
2017-02-22 23:09:59 +0800

    In development

--- spider/index.js
+++ spider/index.js
@@ -1,23 +1,15 @@
 const superagent = require('superagent');
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
+const eventproxy = require('eventproxy');
 
+const urlPageLength = 20;
 
 // set params
-let entryUrl = 'http://i.xiami.com/musician/artists?spm=a1z1s.7400858.1392350033.4.9rgvv0&genre=16&gender=ALL&location=ALL&order=0&page=1&json=1';
-let entryParams = {
-	spm: 'a1z1s.7400858.1392350033.4.9rgvv0',
-	genre: '16',
-	gender: 'ALL',
-	location: 'ALL',
-	order: '0',
-	page: '2',
-	json: '1'
-}
-
 let _singers = [];
+let ep = new eventproxy();
 
 function entry () {
-	for(let i=1; i<=10; i++) {
+	for(let i=1; i<=urlPageLength; i++) {
 		let url = 'http://i.xiami.com/musician/artists?spm=a1z1s.7400858.1392350033.4.9rgvv0&genre=16&gender=ALL&location=ALL&order=0&page=' + i + '&json=1';
 		spiderEntry(url);
 	}
@@ -26,15 +18,11 @@ function entry () {
 /**
  * the entry of the spider
  * @return {}
- */
+ */ 
 function spiderEntry(url) {
 	superagent
 		.get(url)
-		// .send(entryParams)
-		// .accept('json')
 		.end(function(err, res) {
-			// console.log(err);
-			// console.log(res);
 			singerNameGet(res.body);
 		});
 }
@@ -52,11 +40,15 @@ function singerNameGet(context) {
 		let href = value.attribs.href;
 		_singers.push(href);
 	})
-	console.log('_singers.length', _singers.length);
+	ep.emit('singerNameGet');
 }
 
-let spider = (function() {
+ep.after('singerNameGet', urlPageLength, function() {
+	console.log(_singers);
+	console.log(_singers, _singers.length);
+})
 
+let spider = (function() {
 	return {
 		init() {
 			entry ();
